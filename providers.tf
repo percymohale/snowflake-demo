e1baad2ebe8a3e32dbee0c76c56d18c3a518e10b
percy Mohale <percymohale@percys-MacBook-Pro.local>
2023-01-17 14:35:24 +0200

    added basic config for snowflake

--- providers.tf
+++ providers.tf
@@ -6,4 +6,15 @@ terraform {
       name = "snowflake-poc"
     }
   }
+  required_providers {
+    snowflake = {
+      source = "Snowflake-Labs/snowflake"
+      version = "0.55.1"
+    }
+  }
+}
+
+
+provider "snowflake" {
+  # Configuration options
 }
