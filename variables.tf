e8c05eb5a2a8ea9e9abb2f764b41dc14bce3ffad
Percy Mohale <percy.mohale@liberty.co.za>
2023-01-20 09:53:06 +0200

    test variables

--- variables.tf
+++ variables.tf
@@ -1,15 +1,15 @@
 variable "account" {
   type    = string
-  default = "MU98543"
+  # default = "MU98543"
 }
 
 variable "username" {
   type    = string
-  default = "tf-cloud-snowflake"
+  # default = "tf-cloud-snowflake"
 }
 
 variable "region" {
   type    = string
-  default = "eu-west-1"
+  # default = "eu-west-1"
 }
 
